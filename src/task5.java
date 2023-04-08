155c07488311f5c7e812725e75ef3e1367e25417
TonyMontana143 <221074@astanait.edu.kz>
2023-04-08 19:29:53 +0600

    Task 5 solved

--- src/task5.java
+++ src/task5.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
+/*
+        @n-inputted number
+        @return-returning fibonacci number
+ */
 public class task5 {
-
-    public static int fib(int n) {
-        if (n <= 1) {
-            return n;
-        } else {
-            return fib(n-1) + fib(n-2);
-        }
-    }
 
     public static void main() {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         System.out.println(fib(n));
     }
+
+    public static int fib(int n) {
+        if (n <= 1) {       //Base case: if number lesser than one, return itself
+            return n;
+        } else {
+            return fib(n-1) + fib(n-2);//Call function to find the sum of two previous numbers
+        }
+    }
+
+
 }
