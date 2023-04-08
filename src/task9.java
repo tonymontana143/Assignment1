e36430c075c406550df308f634861fce1dabc56e
TonyMontana143 <221074@astanait.edu.kz>
2023-04-08 19:45:50 +0600

    Task 9 solved

--- src/task9.java
+++ src/task9.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;
-
+/*
+    @n,k-inputted numbers
+    @return- returning binomial coefficient
+ */
 public class task9 {
     public static void main() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
-        System.out.println(binomialCoef(n,k));
+        System.out.println(findBinomialCoefficent(n,k));
     }
 
-    public static int binomialCoef(int n, int k) {
-        if (k == 0 || k == n) {
+    public static int findBinomialCoefficent(int n, int k) {
+        if (k == 0 || k == n) {     //Base case: if k is 0 or equal n, return 1
             return 1;
         } else {
-            return binomialCoef(n - 1, k - 1) + binomialCoef(n - 1, k);
+            return findBinomialCoefficent(n - 1, k - 1) + findBinomialCoefficent(n - 1, k);     //Recursively call method with n-1  and k-1, and n-1 and k, and sum results
         }
     }
 }
