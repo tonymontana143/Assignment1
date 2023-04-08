94249e466e8ef0da63862e4d75dcbda62bbfe247
TonyMontana143 <221074@astanait.edu.kz>
2023-04-08 19:41:39 +0600

    Task 7 solved

--- src/task7.java
+++ src/task7.java
@@ -1,25 +1,32 @@
 import java.util.Scanner;
-
+/*
+        @arr-array
+        @n-number of elements
+        @arr[first]-first element of array
+        @arr[last]-last element of array
+ */
 public class task7 {
-    public static void reverse(int[] arr, int first, int last) {
-        if (first >= last) {
-            return;}
-        int temp = arr[first];
-        arr[first] = arr[last];
-        arr[last] = temp;
-
-        reverse(arr, first + 1, last - 1);
-    }
     public static void main() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = sc.nextInt();      //input array
         }
-        reverse(arr, 0, n - 1);
+        WriteReverseOfArray(arr, 0, n - 1);
         for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i] + " ");     //print reversed array
         }
     }
+    public static void WriteReverseOfArray(int[] arr, int first, int last) {
+        if (first >= last) {        //Base case
+            return;
+        }
+        int temp = arr[first];      //Swap first and last element
+        arr[first] = arr[last];
+        arr[last] = temp;
+
+        WriteReverseOfArray(arr, first + 1, last - 1);      //Call function and swap our elements from beginning to end
+    }
+
 }
