895d21166988abe53dee63eda6acfc7947345bfd
TonyMontana143 <221074@astanait.edu.kz>
2023-04-08 19:56:58 +0600

    Task 10 solved

--- src/task10.java
+++ src/task10.java
@@ -1,18 +1,23 @@
 import java.util.Scanner;
+/*
+        @a,b-inputted numbers
+        @res-GCD of numbers
+        @return- returning result
+ */
 public class task10 {
     public static void main() {
         Scanner scanner=new Scanner(System.in);
         int b = scanner.nextInt();
-        int a = scanner.nextInt();
+        int a = scanner.nextInt();      //input numbers
         int res=findGCD(a,b);
         System.out.println(res);
     }
 
     public static int findGCD(int a, int b) {
-        if (b == 0) {
+        if (b == 0) {   //Base case, if b equal to 0
             return a;
         } else {
-            return findGCD(b, a % b);
+            return findGCD(b, a % b);  //Call function with a%b(remainder) and second element, and return result
         }
     }
 }
