a0249218e488e584d19775f39aebc201a734921d
TonyMontana143 <221074@astanait.edu.kz>
2023-04-08 19:24:39 +0600

    Task 4 solved

--- src/task4.java
+++ src/task4.java
@@ -1,18 +1,23 @@
 import java.util.Scanner;
+/*
+        @result-definition of factorial
+        @n-inputted number
+        @return-returning factorial
+ */
 
 public class task4 {
     public static void main() {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int result = factorial(n);
+        int result = FindFactorial(n);
         System.out.println(result);
     }
 
-    public static int factorial(int n) {
-        if (n == 0 || n == 1) {
+    public static int FindFactorial(int n) {
+        if (n == 0 || n == 1) {     //Base case: Factorial of 0 and 1 equal to one
             return 1;
         } else {
-            return n * factorial(n - 1);
+            return n * FindFactorial(n - 1);    //Decrease our number and multiply with each other
         }
     }
 }
