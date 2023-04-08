157399da338d3271eab3f64aa3a9f2c2fe878f39
TonyMontana143 <221074@astanait.edu.kz>
2023-04-08 19:20:55 +0600

    Task 3 solved

--- src/task3.java
+++ src/task3.java
@@ -1,26 +1,33 @@
 import java.util.Scanner;
-
+/*
+        @n-inputted number
+        @return-returning False or True, since we have boolean
+        @i-square root of number
+ */
 public class task3 {
 
     public static void main() {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
 
-        if (isPrime(n, (int) Math.sqrt(n))){ System.out.println("Prime");}
+        if (isPrime(n, (int) Math.sqrt(n)))//check our number by function
+        {
+            System.out.println("Prime");
+        }
 
             else{
-            System.out.println("Composite");
+            System.out.println("Composite");//Otherwise, our number is composite
         }
     }
 
     public static boolean isPrime(int n, int i) {
         if (i == 1) {
-            return true;
+            return true;    //Base case: if sqrt of our number equal to one
         } else {
-            if (n % i == 0) {
+            if (n % i == 0) {   //Divide our number by sqrt of number
                 return false;
             } else {
-                return isPrime(n, i - 1);
+                return isPrime(n, i - 1);   //Call function, and decrease divider by minus one
             }
         }
     }
